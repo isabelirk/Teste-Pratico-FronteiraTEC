47997ce1eb99ced83cf7e56937da394414fdcd0f
isabelirk <isabelireik2@gmail.com>
2023-04-15 16:10:04 -0300

    exercício 1

---
+++ Exercicio1/index.js
@@ -0,0 +1,7 @@
+let nota1 = window.prompt("Digite a primeira nota do aluno:");
+let nota2 = window.prompt("Digite a segunda nota do aluno:");
+let nota3 = window.prompt("Digite a terceira nota do aluno:");
+
+let media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10;
+
+alert("O resultado da média ponderada do aluno é: " + media + "!");
